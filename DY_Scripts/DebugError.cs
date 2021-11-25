5473d6b644f71370fa36d8624147238a48900794
DESKTOP-PR6TQC3\ChoiDaeYoung <chleodud1410@gmail.com>
2021-11-25 17:46:57 +0900

    [update] Log - LogError로 변경

--- DY_Scripts/DebugError.cs
+++ DY_Scripts/DebugError.cs
@@ -4,26 +4,26 @@ public class DebugError
 {
     public static void GetData(string where, string error)
     {
-        Debug.Log("<color=red>Error</color> - " + where + "\nFailed to GetData : " + error);
+        Debug.LogError("<color=red>Error</color> - " + where + "\nFailed to GetData : " + error);
     }
 
     public static void Parse(string where, string error)
     {
-        Debug.Log("<color=red>Error</color> - " + where + "\nFailed to Parse : " + error);
+        Debug.LogError("<color=red>Error</color> - " + where + "\nFailed to Parse : " + error);
     }
 
     public static void Contain(string where, string item)
     {
-        Debug.Log("<color=red>Error</color> - " + where + "\nThere is no " + item);
+        Debug.LogError("<color=red>Error</color> - " + where + "\nThere is no " + item);
     }
 
     public static void LoadError(string where, string pass)
     {
-        Debug.Log("<color=red>Error</color> - " + where + "\nFailed to Load - path : " + pass);
+        Debug.LogError("<color=red>Error</color> - " + where + "\nFailed to Load - path : " + pass);
     }
 
     public static void InstantiateError(string where, string pass)
     {
-        Debug.Log("<color=red>Error</color> - " + where + "\nFailed to Instantiate - path : " + pass);
+        Debug.LogError("<color=red>Error</color> - " + where + "\nFailed to Instantiate - path : " + pass);
     }
 }
