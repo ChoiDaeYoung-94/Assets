0170557456b15d4a55b5c7110d78da0910198948
ChoiDaeYoung <chleodud1410@gmail.com>
2022-01-20 00:11:55 +0900

    [New, Update] 그간 바뀐 내용들

--- DY_Scripts/Managers/Managers.cs
+++ DY_Scripts/Managers/Managers.cs
@@ -13,8 +13,17 @@ public class Managers : MonoBehaviour
     static Managers instance;
     public static Managers Instance { get { return instance; } }
 
+    DataManager _dataM = new DataManager();
+    public static DataManager DataM { get { return instance._dataM; } }
+
+    PoolManager _poolM = new PoolManager();
+    public static PoolManager PoolM { get { return instance._poolM; } }
+
     ResourceManager _resourceM = new ResourceManager();
     public static ResourceManager ResourceM { get { return instance._resourceM; } }
+
+    UpdateManager _updateM = new UpdateManager();
+    public static UpdateManager UpdateM { get { return instance._updateM; } }
 
     private void Awake()
     {
@@ -34,6 +43,9 @@ public class Managers : MonoBehaviour
 
             DontDestroyOnLoad(go);
             instance = go.GetComponent<Managers>();
+
+            DataM.Init();
+            PoolM.Init();
         }
         else
             Destroy(gameObject);
@@ -42,6 +54,31 @@ public class Managers : MonoBehaviour
     private void OnDestroy()
     {
         instance = null;
+    }
+
+    private void Update()
+    {
+        _updateM.OnUpdate();
+    }
+
+    /// <summary>
+    /// 추후 다른 씬 특히 QA 전용 씬을 만들던지 할 때
+    /// flow를 대비하여
+    /// </summary>
+    public void InitM()
+    {
+        DataM.Init();
+        PoolM.Init();
+    }
+
+    /// <summary>
+    /// 씬 전환 시 클리어
+    /// TODO : Resources.UnloadUnusedAssets() 씬 전환 시 추가 해야 함
+    /// </summary>
+    public void Clear()
+    {
+        UpdateM.Clear();
+        //PoolM.Clear();
     }
 
 #if UNITY_EDITOR
--- DY_Scripts/Managers/ResourceManager.cs
+++ DY_Scripts/Managers/ResourceManager.cs
@@ -5,7 +5,7 @@ public class ResourceManager
     public T Load<T>(string where, string path) where T : Object
     {
         if (Resources.Load<T>(path) == null)
-            DebugError.InstantiateError(where, path);
+            DebugError.LoadWarning(where, path);
 
         return Resources.Load<T>(path);
     }
@@ -15,18 +15,10 @@ public class ResourceManager
         GameObject go = Load<GameObject>(where, "Prefabs/" + path);
         if (go == null)
         {
-            DebugError.InstantiateError(where, path);
+            DebugError.InstantiateWarning(where, path);
             return null;
         }
 
         return Object.Instantiate(go, parent);
-    }
-
-    public void Destroy(GameObject go)
-    {
-        if (go == null)
-            return;
-
-        Object.Destroy(go);
     }
 }
